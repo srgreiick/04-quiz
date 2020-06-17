962e1c9ba975b09eff0b0ddb25643f27203559ec
sgreilick <samgreilick@gmail.com>
2020-06-17 09:01:20 -0600

    Initial push, currently has basic css, html, and javascript to control buttons

---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+
+
+//use the speed reader
+//High scores will be stored in local storage
+//
+
+// <!-- need a start button
+// Thinking i can use object and key pairs for q and a, although I don't like this, not very dry.
+// insted of boleans might be able to do a answer question pair insted of a question bolean pair
+// Remember to use JSON.stringify() and JSON.parse() to store and retrieve data for local storage
+
+//  -->
+
+//When I press the start button, the start button diapears
+var startBtn = document.getElementById("startbtn");
+var confirmBtn = document.getElementById("confirmBtn");
+var previousBtn = document.getElementById("previousBtn");
+startBtn.addEventListener("click", startQuiz);
+function startQuiz() {
+    startBtn.className = "hideBtn";
+    confirmBtn.className="btn btn-primary";
+    previousBtn.className="btn btn-primary";
+    console.log("Start Button Hidden");
+}
+function next(params) {
+    
+}
+
+//Then a question is displayed
+//Then the timer is started
+// 
