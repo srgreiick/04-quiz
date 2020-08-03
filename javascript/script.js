80ed4d29a3e60d051bcc67afe3b9cff51e5a26a6
sgreilick <samgreilick@gmail.com>
2020-08-02 22:27:23 -0600

    added grade function and end state for quiz that generates a final score variable, this quiz is totally adaptable to variable numbers of questions and answers

--- javascript/script.js
+++ javascript/script.js
@@ -3,7 +3,6 @@
 // <!-- need a start button
 // Thinking i can use object and key pairs for q and a, although I don't like this, not very dry.
 // insted of ON.parse() to store and retrieve data for local storage
-
 //  -->
 
 //When I press the start button, the start button diapears
@@ -17,14 +16,15 @@ let selectedAnswer = ""
 
 //variable to track current question
 let whichQuestion = 0;
+let score = 0;
 
 startBtn.addEventListener("click", startQuiz);
 
-//starts quze and hides/shows relevent buttons
+//starts quiz and hides/shows relevant buttons
 function startQuiz() {
-    startBtn.className ="hideBtn";
-    confirmBtn.className="btn btn-primary";
-    previousBtn.className="btn btn-primary";
+    startBtn.className = "hideBtn";
+    confirmBtn.className = "btn btn-primary";
+    previousBtn.className = "btn btn-primary";
     console.log("Start Button Hidden");
     nextQuestion();
 
@@ -33,67 +33,67 @@ function startQuiz() {
 
 //Array of objects that will serve as questions for the test
 let questions = [{
-    description : "Select 'Hello'",
-    answer : "Hello",
-    answers :
-    [
-    "asdf",
-    "asdf",
-    "Hello",
-    "asdfff"
-    ],
+    description: "Select 'Hello'",
+    answer: "Hello",
+    answers:
+        [
+            "asdf",
+            "asdf",
+            "Hello",
+            "asdfff"
+        ],
     picked: ""
 },
 {
-    description : "Select 'yes'",
-    answer : "yes",
-    answers :
-    [
-    "asdasdffff",
-    "asasdfffdf",
-    "Hello",
-    "asdfasdfafsdfasdfadsfadsff"
-    ],
+    description: "Select 'yes'",
+    answer: "yes",
+    answers:
+        [
+            "asdasdffff",
+            "asasdfffdf",
+            "Hello",
+            "asdfasdfafsdfasdfadsfadsff"
+        ],
     picked: ""
 },
 {
-    description : "Select 'no'",
-    answer : "no",
-    answers :
-    [
-    "asdasdffff",
-    "lolololo",
-    "Hello",
-    "amymyfadsff"
-    ],
+    description: "Select 'no'",
+    answer: "no",
+    answers:
+        [
+            "asdasdffff",
+            "lolololo",
+            "Hello",
+            "amymyfadsff"
+        ],
     picked: ""
 }
 ];
 
-//console.log("number of answers in current question "+questions[whichQuestion].answers.length)
-console.log("NUmber questions "+questions.length)
-console.log("Current question " +whichQuestion)
-//console.log("number of answers in current question "+ JSON.stringify(questions[whichQuestion].answers))
-
 //Actions tied to the Confirm button
 confirmBtn.addEventListener("click", next);
 
+//Checks if quiz is over based on text content of confirm button
+//Increments whichQuestion variable
 
 function next() {
-    console.log("%cCurrent question " +whichQuestion, "color:red");
+    if (confirmBtn.textContent === "Finish") {
+        console.log("QUiz OVA");
+        grade();
+        return
+    }
+
     whichQuestion++
+
     if (whichQuestion >= questions.length) {
-        whichQuestion = (questions.length) -1;
+        whichQuestion = (questions.length) - 1;
         confirmBtn.textContent = "Finish"
     }
     else {
         confirmBtn.textContent = "Confirm"
-        previousBtn.className="btn btn-primary";
+        previousBtn.className = "btn btn-primary";
     };
-    selectedAnswer = 
-
     nextQuestion();
-    console.log("%cCurrent question " +whichQuestion, "color:red");
 }
 
 
@@ -106,29 +106,30 @@ function next() {
 
 
 function nextQuestion() {
-    console.log("%cCurrent question " +whichQuestion, "color:red");
     $(".answerBtn").remove();
     questionDescription.textContent = questions[whichQuestion].description
 
     //For loop that iterates through questions array and creates a new button for each one
     for (let i = 0; i < questions[whichQuestion].answers.length; i++) {
-        console.log(questions[whichQuestion].answers[i]);
         let answerOptions = document.createElement("a");
         answerOptions.className = "btn btn-primary answerBtn";
-        answerOptions.id = "answer "+i;
-        answerOptions.textContent = JSON.stringify(questions[whichQuestion].answers[i]);
-        console.log(answerOptions.textContent);
+        answerOptions.id = "answer " + i;
+        answerOptions.textContent = questions[whichQuestion].answers[i];
         questionLocation.appendChild(answerOptions);
     }
-    questionLocation.addEventListener('click',(e)=>{
+
+    questionLocation.addEventListener('click', (e) => {
         selectedAnswer = e.target.textContent
         questions[whichQuestion].picked = selectedAnswer
         console.log(questions[whichQuestion].picked + " picked")
     })
+    for (let i = 0; i < questions.length; i++) {
+        console.log(questions[i].picked)
+    }
 };
 
 //Actions tied to the specific answer buttons
-document.getElementById("questionLocation").addEventListener("click", function(e) {
+document.getElementById("questionLocation").addEventListener("click", function (e) {
     console.log(e.currentTarget);  //<ul><li>...</li><ul>
     console.log(e.target);         //<a href="#">Item 2</a>
 
@@ -142,32 +143,43 @@ document.getElementById("questionLocation").addEventListener("click", function(e
 //Actions tied to the previous button
 previousBtn.addEventListener("click", previous);
 function previous() {
-    console.log("%Current question " +whichQuestion, "color:red")
-   
+    console.log("%Current question " + whichQuestion, "color:red")
+
     whichQuestion--;
     if (whichQuestion < 0) {
         whichQuestion = 0;
         confirmBtn.textContent = "Confirm";
-        previousBtn.className="hideBtn";
+        previousBtn.className = "hideBtn";
     }
-    else{
+    else {
         confirmBtn.textContent = "Confirm"
     };
     previousAction()
-    console.log("%cCurrent question " +whichQuestion, "color:red")
-}; 
+    console.log("%cCurrent question " + whichQuestion, "color:red")
+};
 
 function previousAction() {
-    console.log("Current question " +whichQuestion)
-    console.log("Previous clicked")
-    console.log("%cCurrent question " +whichQuestion, "color:red");
     nextQuestion();
-    console.log("%cCurrent question " +whichQuestion, "color:red");
 };
 
 
+function grade() {
+    for (let i = 0; i < questions.length; i++) {
+        if (questions[i].answer == questions[i].picked) {
+            score++
+        }
+        else {
+            console.log("ya missed")
+        }
+        console.log(questions[i].picked + questions[i].answer)
+    }
 
 
+
+
+    console.log("You got " + score + "/" + questions.length)
+
+}
 
 
 
